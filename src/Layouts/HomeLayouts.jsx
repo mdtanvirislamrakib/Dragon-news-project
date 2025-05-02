d08f32bcc27e01a6d48fc7220b0affa83fb76464
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-05-02 10:38:49 +0600

    marquee section added

--- src/Layouts/HomeLayouts.jsx
+++ src/Layouts/HomeLayouts.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from '../components/Header/Header';
+import LatestNews from '../components/LatestNews/LatestNews';
 
 const HomeLayouts = () => {
     return (
         <div>
             <header>
                 <Header></Header>
+                <section className='w-11/12 mx-auto my-3'>
+                    <LatestNews></LatestNews>
+                </section>
             </header>
 
             <main>
